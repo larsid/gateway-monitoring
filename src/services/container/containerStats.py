af50f7f0bf35eb84d8ea3612b3bd8e8374f53f3b
AllanCapistrano <allan.capistrano3@gmail.com>
2022-08-29 17:36:24 -0300

    Refactor: split getContainerStats into two methods

--- src/services/container/containerStats.py
+++ src/services/container/containerStats.py
@@ -11,12 +11,30 @@ CPU_POSITION = 2
 MEM_POSITION = 3
 # ---------------------------------------------------------------------------- #
 
-def getContainerStats(container_id: str) -> Dict[str, str]:
+def runDockerStats() -> str:
+    """ Executa o comando `docker stats` e retorna a sua resposta.
+
+    Returns
+    -------
+    output: :class:`str`
+    """
+
+    try:
+        output = run(["docker", "stats", "--no-stream"]).output
+        
+        return str(output)
+    except:   
+        print("Erro ao tentar verificar o estado do container! Verifique se o Docker está executando.")
+        exit()
+
+def getContainerStats(output: str, container_id: str) -> Dict[str, str]:
     """ Retorna o ID, a porcentagem de utilização da CPU e a porcentagem de
     utilização da memória RAM de um container com base no ID especificado.
 
     Parameters
     ----------
+    output: :class: `str`
+        Resposta do comando `docker stats`.
     container_id: :class: `str`
         ID do container que deseja saber o estado.
 
@@ -27,26 +45,20 @@ def getContainerStats(container_id: str) -> Dict[str, str]:
 
     container_stat: Dict[str, str] = {}
 
-    try:
-        output = run(["docker", "stats", "--no-stream"]).output
-    except:   
-        print("Erro ao tentar verificar o estado do container! Verifique se o Docker está executando.")
-        exit()
-    else:
-        # Sanitizando o retorno do comando.
-        temp: str       = str(output).replace("b'", "")
-        containers: str = sub(r"\s+", " " , temp)
+    # Sanitizando o retorno do comando.
+    temp: str       = output.replace("b'", "")
+    containers: str = sub(r"\s+", " " , temp)
 
-        # Dividindo o retorno do comando pelas linhas.
-        container_lines: list = containers.split("\\n")
+    # Dividindo o retorno do comando pelas linhas.
+    container_lines: list = containers.split("\\n")
 
-        for container_line in container_lines:
-            # Procurando o container com o ID especificado.
-            if(container_line.find(container_id) != -1):
-                container_info: List[str] = container_line.split(" ")
-                
-                container_stat["id"]     = container_info[ID_POSITION]
-                container_stat["cpu"]    = container_info[CPU_POSITION]
-                container_stat["memory"] = toGibibyte(container_info[MEM_POSITION])
-                
-                return container_stat
+    for container_line in container_lines:
+        # Procurando o container com o ID especificado.
+        if(container_line.find(container_id) != -1):
+            container_info: List[str] = container_line.split(" ")
+            
+            container_stat["id"]     = container_info[ID_POSITION]
+            container_stat["cpu"]    = container_info[CPU_POSITION]
+            container_stat["memory"] = toGibibyte(container_info[MEM_POSITION])
+            
+            return container_stat
