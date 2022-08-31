e8506428b43bf592738713ed6e426c2fc05aa236
AllanCapistrano <allan.capistrano3@gmail.com>
2022-08-31 09:29:48 -0300

    Feat: add getGatewayLoadRate import

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -3,3 +3,4 @@ from .machine.memoryCheck import memoryUsageCheck
 from .csv.csvWriter import CsvWriter
 from .container.containerId import getContainerIds
 from .container.containerStats import getContainerStats, runDockerStats
+from .container.gatewayLoad import getGatewayLoadRate
