5a1615fa75bb3b2e7edae971791b12295fa701e9
AllanCapistrano <allan.capistrano3@gmail.com>
2022-09-01 13:04:28 -0300

    Feat: add getGatewayName import

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -4,3 +4,4 @@ from .csv.csvWriter import CsvWriter
 from .container.containerId import getContainerIds
 from .container.containerStats import getContainerStats, runDockerStats
 from .container.gatewayLoad import getGatewayLoadRate
+from .container.gatewayName import getGatewayName
