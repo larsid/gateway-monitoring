9efd562c0187c7e51cdd8d6c50b5479159ad69fb
AllanCapistrano <allan.capistrano3@gmail.com>
2022-09-24 23:15:41 -0300

    Refactor: update 'free' command parameters

--- src/services/machine/memoryCheck.py
+++ src/services/machine/memoryCheck.py
@@ -1,25 +1,15 @@
 from os import popen
 
-def memoryUsageCheck(rounding_number: int = 2) -> float:
-    """ Retorna a taxa de utilização da memória RAM em porcentagem.
-
-    Parameters
-    ----------
-    rounding_number: :class:`int`
-        Número de máximo de casas de decimais.
+def memoryUsageCheck() -> str:
+    """ Retorna a utilização da memória RAM
 
     Returns
     -------
-    memory_usage_percentage: :class:`float`
+    used_memory: :class:`str`
     """
 
     (total_memory, used_memory, free_memory) = map(
-        int, popen('free -t -m').readlines()[-1].split()[1:]
+        str, popen('free -t -h').readlines()[-1].split()[1:]
     )
 
-    memory_usage_percentage: float = round(
-        (used_memory/total_memory) * 100, 
-        rounding_number
-    )
-
-    return memory_usage_percentage
+    return used_memory
