e5a6ec55032ba3841c6e64f77322779f341284dd
AllanCapistrano <allan.capistrano3@gmail.com>
2022-09-26 19:48:16 -0300

    Feat: add gibibyteToGigabyte method

---
+++ src/services/convert/gibibyteToGigabyte.py
@@ -0,0 +1,29 @@
+from re import findall, search
+
+# ------------------------------ Constants ----------------------------------- #
+ROUNDING_NUMBER = 2
+# ---------------------------------------------------------------------------- #
+
+def gibibyteToGigabyte(value: str) -> str:
+    """ Converte um valor em Gibibyte para Gigabyte
+
+    Parameters
+    ----------
+    value: :class:`str`
+        Valor em Gibibyte.
+
+    Returns
+    -------
+    valueConverted: :class:`str`
+    """
+
+    if (value.find(".") != -1):
+        # Pegando somente a parte racional da String.
+        temp: float = float(findall("\d+\.\d+", value)[-1])
+    else:
+        # Pegando somente a parte inteira da String.
+        temp: int = int(search(r'\d+', value).group())
+
+    valueConverted: str = f"{round(temp * 1.07374, ROUNDING_NUMBER)}Gi"
+
+    return valueConverted
